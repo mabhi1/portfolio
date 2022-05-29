f4347d818dfb2fe8ff52f7923d81d8fa4209089f
Abhishek Mishra <amishr1@stevens.edu>
2022-05-28 21:57:02 -0400

    scrolltotop

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
+import smoothscroll from "smoothscroll-polyfill";
 
 function MyApp({ Component, pageProps }) {
+    // kick off the polyfill!
+    smoothscroll.polyfill();
     return (
         <div className="bg-slate-50 dark:bg-slate-900">
             <Navbar />
