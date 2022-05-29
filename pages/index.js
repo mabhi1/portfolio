5b38c8be67495f87b641ac00a66fc47b7fc938b9
Abhishek Mishra <amishr1@stevens.edu>
2022-05-28 22:18:32 -0400

    scrolltotop

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
                             Scroll to top
                         </span>
                         <AiOutlineArrowUp
-                            className="fixed z-40 hover:-translate-y-1 animate-[scrolltotop_500ms_ease-in-out] bottom-12 right-12 text-5xl rounded-full cursor-pointer p-[10px] shadow-[0px_0px_6px_1px_rgb(21,94,117,0.6)] hover:shadow-[0px_0px_8px_3px_rgb(21,94,117,0.6)] scale-110 bg-cyan-600 dark:bg-cyan-700 text-slate-50 transition duration-200"
+                            className="fixed z-40 hover:-translate-y-1 animate-[scrolltotop_500ms_ease-in-out] bottom-12 right-12 text-5xl rounded-full cursor-pointer p-[10px] scale-110 bg-cyan-600/90 dark:bg-cyan-700 text-slate-50 transition duration-200"
                             onClick={() => {
                                 window?.scrollTo(0, 0);
                             }}
